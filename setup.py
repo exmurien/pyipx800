4fa2a3a80f91c4769f554c309db3407bd6140a3f
slashx57 <boiselet@free.fr>
2020-03-01 11:33:05 +0100

    correct projet information for pypi

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="HA_teleinfo-slashx57", 
+    name="pyipx800",
     version="0.0.1",
     author="slashx57",
     author_email="boiselet@free.fr",
-    description="Teleinfo sensor package for HomeAssistant",
+    description="Package to control IPX800v4 from GCEElectronics",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/slashx57/pyipx800",
